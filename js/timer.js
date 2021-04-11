3902220dd039811cdea6b5def942658931cc0e29
Vlad Safin <djutsu17@gmail.com>
2021-04-11 18:49:51 +0300

    code corrections

--- js/timer.js
+++ js/timer.js
@@ -1,69 +1,73 @@
+const arrayOfSpans = document.querySelectorAll('.value');
+        
+
 class CountdownTimer {
-    constructor({selector, targetDate}) {
-        this.element = document.querySelector(selector);
+    constructor({selector, targetDate, name, emoji}) {
+        this.selector = selector;
         this.targetDate = targetDate;
-        this.currentDate;
-        this.date;
+        this.name = name;
+        this.emoji = emoji;
+        
+        
     }
+
     myBirthday() {
-        this.element.insertAdjacentHTML('afterbegin', '<p class="title">&#10024До моего дня рождения осталось:</p>');
+        const titleTimer = document.querySelector(this.selector);
+        
+        titleTimer.insertAdjacentHTML('beforebegin', `<p class="title"> ${this.emoji} До дня рождения ${this.name} осталось:</p>`);
         this.getDate();
+
     }
+
     getDate() {
         this.targetDate = new Date(this.targetDate).getTime();
         this.getTimer();
     }
 
     getTimer() {
-        const interval = setInterval(() => {
+        this.interval = setInterval(() => {
             this.currentDate = Date.now();
             this.date = this.targetDate - this.currentDate;
             
             if (this.date <= 0) {
                 this.date = 0;
-                clearInterval(interval);
+                clearInterval(this.interval);
             }
             
             
             this.calculate(this.date);
         }, 1000);
     }
+
     
     calculate() {
-        const refs = {
-            days: document.querySelector(`
-            [data-value="days"]`),
-            hours: document.querySelector(`
-            [data-value="hours"]`),
-            mins: document.querySelector(`
-            [data-value="mins"]`),
-            secs: document.querySelector(`
-            [data-value="secs"]`)
-        }
-
+        
         const days = Math.floor(this.date / (1000 * 60 * 60 * 24));
-        refs.days.textContent = `${days}`;
-        
         const hours = this.pad(Math.floor((this.date % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        refs.hours.textContent = `${hours}`;
-        
         const mins = this.pad(Math.floor((this.date % (1000 * 60 * 60)) / (1000 * 60)));
-        refs.mins.textContent = `${mins}`;
-        
         const secs = this.pad(Math.floor((this.date % (1000 * 60)) / 1000));
-        refs.secs.textContent = `${secs}`;
+        
+        const arrayOfData = [days, hours, mins, secs];
+        arrayOfSpans.forEach((el, i) => el.textContent = arrayOfData[i]);
+        
     }
     
-    pad(time) {
-        return String(time).padStart(2, '0');
+    pad(value) {
+        return String(value).padStart(2, '0');
     }
+
+    
 }
 
-
-const timer = new CountdownTimer({
+const vlad = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('May 20, 2021'),
+  name: 'Vlad',
+  emoji: '&#10024',
    
 });
 
-timer.myBirthday();
+vlad.myBirthday();
+
+
+
